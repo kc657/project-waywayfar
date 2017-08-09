ad3f42cdb3e0f7802fc80b8a28bee565f501d672
kc657 <kvn.0218@gmail.com>
2017-08-09 14:55:30 -0700

    initial commit

---
+++ src/Home.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import './Home.css';
+
+class Home extends Component {
+  // Main home page component
+}
+
+export default Home;
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Home from './Home';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(<Home />, document.getElementById('root'));
+registerServiceWorker();
