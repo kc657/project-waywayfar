9c55cdf51d63887ae8717da16666b59c5fb4c975
kc657 <kvn.0218@gmail.com>
2017-08-14 13:00:05 -0700

    Merge error deletes

--- src/body/AllTitles.js
+++ src/body/AllTitles.js
@@ -8,13 +8,8 @@ class AllTitles extends Component {
           <div className='col s3'>
             <h4>Cities</h4>
           </div>
-<<<<<<< HEAD
-          <div className='col s8 offset-s1 left-align'>
-            <h4>Shanghai</h4>
-=======
           <div className="col s8 offset-s1 left-align">
             <h4>{this.props.selectedCityObj.name}</h4>
->>>>>>> 4ba93421edc42f4b99dcb3c2552d931168384a80
           </div>
         </div>
       </div>
--- src/body/SinglePost.js
+++ src/body/SinglePost.js
@@ -3,28 +3,11 @@ import React, {Component} from 'react'
 class SinglePost extends Component {
 
   render () {
-<<<<<<< HEAD
-    return (
-      <div>
-        {this.props.allPosts.map((item, index) => (
-          <div className='col s12 card-panel hoverable'>
-            <div data-post-id={item._id} className='individualPost'>
-              <div id={item._id} className='col m3'>
-                <img className='responsive-img post-img' src={item.image} />
-              </div>
-              <div className='col m7'>
-                <h5>{item.title}</h5>
-                <p>{item.text}</p>
-              </div>
-              <div className='col m2 right'>
-                <button type='delete' onClick={this.props.handleDelete} className='singlePostfunctions waves-effect waves-red btn-sm btn-flat'>Delete</button>
-                <button className='singlePostfunctions waves-effect waves-teal btn-sm btn-flat hoverable'>Edit</button>
-=======
     let selectedPosts = this.props.selectedPosts.map( post => {
       return(
         <div className='col s12 card-panel hoverable'>
           <div data-post-id={item._id} className='individualPost'>
-        
+
             <div id={post._id} className='col m4'>
               <img className='responsive-img post-img' src={post.image} />
             </div>
@@ -35,7 +18,6 @@ class SinglePost extends Component {
              <div className='col m2 right'>
                 <button type='delete' onClick={this.props.handleDelete} className="singlePostfunctions waves-effect waves-red btn-sm btn-flat">Delete</button>
                 <button className="singlePostfunctions waves-effect waves-teal btn-sm btn-flat hoverable">Edit</button>
->>>>>>> 4ba93421edc42f4b99dcb3c2552d931168384a80
               </div>
             </div>
           </div>
