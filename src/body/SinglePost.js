72eb423205d915dd691b1ae218e54cffdedbe1e0
kc657 <kvn.0218@gmail.com>
2017-08-14 13:01:19 -0700

    SinglePost div error debugged

--- src/body/SinglePost.js
+++ src/body/SinglePost.js
@@ -3,10 +3,10 @@ import React, {Component} from 'react'
 class SinglePost extends Component {
 
   render () {
-    let selectedPosts = this.props.selectedPosts.map( post => {
-      return(
+    let selectedPosts = this.props.selectedPosts.map(post => {
+      return (
         <div className='col s12 card-panel hoverable'>
-          <div data-post-id={item._id} className='individualPost'>
+          <div data-post-id={post._id} className='individualPost'>
 
             <div id={post._id} className='col m4'>
               <img className='responsive-img post-img' src={post.image} />
@@ -14,16 +14,15 @@ class SinglePost extends Component {
             <div className='col m8'>
               <h5>{post.title}</h5>
               <p>{post.text}</p>
-             </div>
-             <div className='col m2 right'>
-                <button type='delete' onClick={this.props.handleDelete} className="singlePostfunctions waves-effect waves-red btn-sm btn-flat">Delete</button>
-                <button className="singlePostfunctions waves-effect waves-teal btn-sm btn-flat hoverable">Edit</button>
-              </div>
+            </div>
+            <div className='col m2 right'>
+              <button type='delete' onClick={this.props.handleDelete} className='singlePostfunctions waves-effect waves-red btn-sm btn-flat'>Delete</button>
+              <button className='singlePostfunctions waves-effect waves-teal btn-sm btn-flat hoverable'>Edit</button>
             </div>
           </div>
         </div>
       )
-    });
+    })
 
     return (
       <div>
