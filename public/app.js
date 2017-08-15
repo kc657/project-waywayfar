a421cc4e9b0844f1598eaa797deef221178acfed
kc657 <kvn.0218@gmail.com>
2017-08-15 09:43:57 -0700

    Removed useless files

--- public/app.js
+++ public/app.js
@@ -1,5 +1,5 @@
- $(document).ready(function () {
-   console.log('Listening for clicks')
-    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
-   $('.modal-trigger').modal()
- })
+ // $(document).ready(function () {
+ //   console.log('Listening for clicks')
+ //    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+ //   $('.modal-trigger').modal()
+ // })
